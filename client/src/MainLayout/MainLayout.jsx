e1d9e3fb5687973d89b14c105b666e5b4240cbe9
iftekhar <princemahmood117@gmail.com>
2025-11-21 12:55:33 +0600

    scroll effect done

--- client/src/MainLayout/MainLayout.jsx
+++ client/src/MainLayout/MainLayout.jsx
@@ -1,7 +1,40 @@
+// import { Outlet } from "react-router-dom";
+// import Navbar from "../Components/Navbar/Navbar";
+// import Footer from "../Components/Footer/Footer";
+// import { useState } from "react";
+// import LoadingScreen from "../Components/LoadingScreen/LoadingScreen";
+
+// const MainLayout = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   const handleLoadingComplete = () => {
+//     setIsLoading(false);
+//   };
+
+//   return (
+//     <>
+//     {isLoading && <LoadingScreen handleLoadingComplete={handleLoadingComplete} />}
+
+//      { !isLoading && <div className="flex flex-col min-h-screen">
+//         <Navbar />
+//         <main className="grow">
+//           <Outlet />
+//         </main>
+//         <Footer />
+//       </div>}
+//     </>
+//   );
+// };
+
+// export default MainLayout;
+
+
+
+import Lenis from "@studio-freight/lenis";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
-import { useState } from "react";
 import LoadingScreen from "../Components/LoadingScreen/LoadingScreen";
 
 const MainLayout = () => {
@@ -11,17 +44,40 @@ const MainLayout = () => {
     setIsLoading(false);
   };
 
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smooth: true,
+      smoothTouch: false,
+      easing: (x) => 1 - Math.pow(1 - x, 2),
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <>
-    {isLoading && <LoadingScreen handleLoadingComplete={handleLoadingComplete} />}
+      {isLoading && <LoadingScreen handleLoadingComplete={handleLoadingComplete} />}
 
-     { !isLoading && <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="grow">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>}
+      {!isLoading && (
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="grow">
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+      )}
     </>
   );
 };
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,9 +1,8 @@
 
-import Navbar from '../../Components/Navbar/Navbar';
+
 import HeroSlider from '../../Components/HeroSlider/HeroSlider';
 import Royal from '../../Components/HeroSlider/Royal';
 import LandmarkProjects from '../../Components/HeroSlider/LandmarkProject/LandmarkProject';
-import EmptyDiv from '../../Components/Footer/EmptyDiv';
 import MapSection from '../../Components/MapSection/MapSection';
 import LegacySlides from '../../Components/LegacySection/LegacySlides';
 import { Helmet } from 'react-helmet-async';
@@ -16,13 +15,11 @@ const Home = () => {
      </Helmet>
      
          <div className="relative">
-            <Navbar></Navbar>
             <HeroSlider></HeroSlider>
             <div className='mt-14'>
               <Royal></Royal>
             </div>
             <LandmarkProjects youtubeId={'kqq0JTjrXlM'}></LandmarkProjects>
-            {/* <EmptyDiv></EmptyDiv> */}
             <MapSection></MapSection>
             <LegacySlides></LegacySlides>
           </div>
